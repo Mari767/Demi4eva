7341b939d9e3ca73fca4dc67c4dddb94e843693b
Mari <mdemiceva3@gmail.com>
2020-10-17 00:12:33 +0300

    4

---
+++ Lab1/project 4/project 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	//  В принте  Введите значение а, б, ...
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	a = 3;
+	b = 4;
+	c = a;
+	a = b;
+	b = c;
+	printf("%d\n %d\n", a, b);
+
+	//  В принте  Введите значение а, б, ...
+	b = a + b;
+	a = b - a;
+	b = b - a;
+	printf("%d\n %d", a, b);
+
+
+
+}
